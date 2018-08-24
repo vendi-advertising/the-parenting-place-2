a8a9e99b5157fe734a402270a88bc39d2ae0c302
adam <adam@adamharpur.com>
2018-08-24 13:50:23 -0500

    feat(master): started aI1ec support

--- page.php
+++ page.php
@@ -19,6 +19,7 @@ get_header();
 <?php
     $default_sidebar_position = get_theme_mod( 'default_sidebar_position', 'right' );
 ?>
+<?php echo do_shortcode("[ai1ec view='monthly']"); ?>
 
 <div class="container">
     <div class="row">
