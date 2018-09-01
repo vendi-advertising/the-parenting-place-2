0126e26b43af53bed5772e1c651a02b8910d2b8b
adam <adam@adamharpur.com>
2018-09-01 09:02:05 -0500

    feat(master): added image support for cards

--- partials/sections/card/standard.php
+++ partials/sections/card/standard.php
@@ -5,9 +5,11 @@ if(count($cards)) : ?>
 	<div class="container">
         <div class="card-columns">
             <?php foreach($cards as $card): ?>
-                
                 <div class="card info bg-<?php echo esc_html_e( $card['color'] ); ?>">
                     <div class="card-body">
+                        <?php if($card['include_image']):?>
+                            <img src="<?php echo esc_html_e( $card['image']['url'] ); ?>" alt="<?php echo esc_html_e( $card['title'] ); ?>">
+                        <?php endif?>
                         <h4 class="card-title"><?php echo esc_html_e( $card['title'] ) ?></h4>
                         <p class="card-text"><?php echo esc_html_e( $card['main_text'] ) ?></p>
                     </div>
