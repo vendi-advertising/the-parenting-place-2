4582dbbefa0d518fb0db82b7d7f878a8674ad734
Adam Harpur <adam@harpur.io>
2018-11-09 14:39:40 -0600

    added fonts and additional styling

--- partials/sections/blog/home.php
+++ partials/sections/blog/home.php
@@ -9,7 +9,14 @@
                     <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
                 </header>
                 <?php endif;
-                    $query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1));
+                    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                    $args = array(
+                                'post_type'=>'post', 
+                                'post_status'=>'publish', 
+                                'posts_per_page'=> 5,
+                                'paged' => $paged,
+                            );
+                    $query = new WP_Query($args);
                     while ( $query->have_posts() ) :
                         $path = dirname(__FILE__, 4) . '/partials/posts/' .  get_post_type($query->the_post()) . '.excerpt.php';
                         include($path);
@@ -18,8 +25,28 @@
                 else :
                     get_template_part( 'partials/posts/', 'none' );
                 endif; ?>
-                <?php echo paginate_links( ); ?>
             </main>
+            <nav>
+                <div class="pagination">
+                    <?php 
+                        echo paginate_links( array(
+                            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+                            'total'        => $query->max_num_pages,
+                            'current'      => max( 1, get_query_var( 'paged' ) ),
+                            'format'       => '?paged=%#%',
+                            'show_all'     => false,
+                            'type'         => 'plain',
+                            'end_size'     => 5,
+                            'mid_size'     => 1,
+                            'prev_next'    => true,
+                            'prev_text'    => sprintf( '<i></i> %1$s', __( '&#10092; Newer Posts', 'text-domain' ) ),
+                            'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts &#10093;', 'text-domain' ) ),
+                            'add_args'     => false,
+                            'add_fragment' => '',
+                        ) );
+                    ?>
+                </div>
+            </nav>
         </section>
     </div>
 <div/>
