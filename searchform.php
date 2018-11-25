947e112ce2a7c970df3877611a71f27ac8f0ecc8
Adam Harpur <adam@harpur.io>
2018-11-25 11:53:13 -0600

    added action for search action

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <div id="sb-search" class="sb-search">
-    <form>
+    <form action="<?php echo home_url( '/' ); ?>">
         <input class="sb-search-input" placeholder="Search" name="s" id="search" type="search">
         <input class="sb-search-submit" value="" type="submit">
         <span class="sb-icon-search"> <i class="fas fa-search" aria-hidden="true"> </i> </span>
